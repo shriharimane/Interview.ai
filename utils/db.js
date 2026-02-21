f1c07ac6f020d0e103a4d03996fda3d8b386943d
Shrihari Mane <138723848+shriharimane@users.noreply.github.com>
2026-02-21 09:46:42 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema'
-const sql = neon("postgresql://ai%20mock%20interview_owner:aQ7GxFrR2qHj@ep-quiet-hat-a5sn0e5z.us-east-2.aws.neon.tech/ai-mock-interview?sslmode=require");
+const sql = neon("");
 export const db = drizzle(sql,{schema});
