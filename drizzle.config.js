3ea21a6ad61a598fb45cd444e83e454a66dd2f5a
Shrihari Mane <138723848+shriharimane@users.noreply.github.com>
2026-02-21 09:47:22 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,7 +5,7 @@ module.exports = {
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
-      url: "postgresql://ai%20mock%20interview_owner:aQ7GxFrR2qHj@ep-quiet-hat-a5sn0e5z.us-east-2.aws.neon.tech/ai-mock-interview?sslmode=require"
+    url : "
     }
   };
   
